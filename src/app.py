ef5ecf6c4400a524fdec35d4a07d91aaaca406a2
mjhs1999 <138990797+mjhs1999@users.noreply.github.com>
2023-12-13 19:49:21 +0000

    I have to push

---
+++ src/app.py
@@ -0,0 +1,31 @@
+from flask import Flask, jsonify, request
+app = Flask(__name__)
+
+todos = [
+    { "label": "My first task", "done": False },
+    { "label": "My second task", "done": False }
+]
+
+@app.route('/todos', methods=['GET'])
+def hello_world():
+    return jsonify(todos)
+
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    request_body = request.get_json(force=True)
+    todos.append(request_body)
+    print("Incoming request with the following body", request_body)
+    return jsonify(todos)
+
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    todos.remove(todos[position])
+    print("This is the position to delete: ",position)
+    return jsonify(todos)
+
+
+
+
+# These two lines should always be at the end of your app.py file.
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=3245, debug=True)
